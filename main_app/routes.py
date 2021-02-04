053d385b8abb434b563490f293329fa25066a93b
Chai <chaiyawahoo@gmail.com>
2021-02-04 15:59:14 -0800

    Revert "Revert "add edit page/route and fix forms""
    This reverts commit e70b06fb158d02289fba180b437b15fd9340b650.

--- main_app/routes.py
+++ main_app/routes.py
@@ -4,6 +4,8 @@ from bson.objectid import ObjectId
 from main_app import app, db
 
 main = Blueprint("main", __name__)
+sexes = [["Not Set", "Select One..."], ["male", "Male"], ["female", "Female"], ["prefer not to say", "Prefer Not To Say"]]
+roles = [["Not Set", "Select One..."], ["developer", "Developer"], ["artist", "Artist"], ["combination", "Both"]]
 
 ############################################################
 # ROUTES
@@ -46,6 +48,30 @@ def create():
         db.users.insert_one(new_user)
         new_user_id = db.users.find_one(new_user)["_id"]
 
-        return redirect(url_for("user"), user_id=new_user_id)
+        return redirect(url_for("main.user"), user_id=new_user_id)
     else:
-        return render_template("create.html")
+        return render_template("create.html", sexes=sexes, roles=roles)
+
+@main.route("/edit/<user_id>", methods=["GET", "POST"])
+def edit(user_id):
+    user_data = db.users.find_one({"_id": ObjectId(user_id)})
+    if request.method == "POST":
+        edited_user_data = {
+            "username": request.form.get("username"),
+            "password": request.form.get("password"),
+            "email": request.form.get("email"),
+            "phone_number": request.form.get("phone_number"),
+            "first_name": request.form.get("first_name"),
+            "last_name": request.form.get("last_name"),
+            "dob": request.form.get("dob"),
+            "sex": request.form.get("sex"),
+            "role": request.form.get("role"),
+            "links": [request.form.get("link")]
+        }
+        for key, value in edited_user_data.items():
+            if value is not user_data[key]:
+                db.users.update_one(user_data, {"$set": {key: value}})
+        
+        return redirect(url_for("main.user", user_id=user_id))
+    else:
+        return render_template("edit.html", user=user_data, sexes=sexes, roles=roles)
