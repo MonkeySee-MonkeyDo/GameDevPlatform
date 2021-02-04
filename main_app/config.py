d4cc06818a18a8f232874bcf35952dba2f4556f4
Chai <chaiyawahoo@gmail.com>
2021-02-03 16:09:07 -0800

    update config

--- main_app/config.py
+++ main_app/config.py
@@ -3,5 +3,10 @@ import os
 
 load_dotenv()
 
+username=os.getenv("MONGO_USERNAME")
+password=os.getenv("MONGO_PASSWORD")
+cluster_url=os.getenv("MONGO_CLUSTER_URL")
+db=os.getenv("MONGO_DB")
+
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_URI = f"mongodb+srv://{username}:{password}@{cluster_url}/{db}?retryWrites=true&w=majority"
