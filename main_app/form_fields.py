c0b1bdca6052071f35c702a09d3d5fadd9a3165a
Chai <chaiyawahoo@gmail.com>
2021-02-07 23:32:20 -0800

    make files more readable

--- main_app/form_fields.py
+++ main_app/form_fields.py
@@ -18,7 +18,12 @@ class FileUploadField(Field):
         return f"""
         <label>
             {self.title}{"*" if self.required else ""}:
-            <input type="file" name="{self.name if self.name else ""}"{' accept="' + ",".join(self.file_types) + '"' if self.file_types else ""}{" required" if self.required else ""}{" disabled" if self.disabled else ""}>
+            <input 
+            type="file" 
+            name="{self.name if self.name else ""}"
+            {' accept="' + ",".join(self.file_types) + '"' if self.file_types else ""}
+            {" required" if self.required else ""}
+            {" disabled" if self.disabled else ""}>
         </label>
         """
 
@@ -32,7 +37,11 @@ class InputField(Field):
         return f"""
         <label>
             {self.title}{"*" if self.required else ""}:
-            <input type="{self.input_type}" name="{self.name if self.name else ""}"{' value="' + self.value + '"' if self.value else ""}{" required" if self.required else ""}{" disabled" if self.disabled else ""}>
+            <input type="{self.input_type}" 
+            name="{self.name if self.name else ""}
+            "{' value="' + self.value + '"' if self.value else ""}
+            {" required" if self.required else ""}
+            {" disabled" if self.disabled else ""}>
         </label>
         """
 
@@ -46,7 +55,13 @@ class TextAreaField(Field):
         return f"""
         <label>
             {self.title}{"*" if self.required else ""}:
-            <textarea row="{self.rows}" cols="{self.cols}" name="{self.name if self.name else ""}"{" required" if self.required else ""}{" disabled" if self.disabled else ""}>{self.value if self.value else ""}</textarea>
+            <textarea 
+            row="{self.rows}" 
+            cols="{self.cols}" 
+            name="{self.name if self.name else ""}"
+            {" required" if self.required else ""}
+            {" disabled" if self.disabled else ""}
+            >{self.value if self.value else ""}</textarea>
         </label>
         """
 
@@ -59,11 +74,17 @@ class SelectField(Field):
         start = f"""
         <label>
             {self.title}{"*" if self.required else ""}:
-            <select name="{self.name if self.name else ""}"{" required" if self.required else ""}{" disabled" if self.disabled else ""}>"""
+            <select 
+            name="{self.name if self.name else ""}"
+            {" required" if self.required else ""}
+            {" disabled" if self.disabled else ""}>"""
         middle = ""
         for option in self.options:
             middle += f"""
-                <option value="{option[0]}"{" selected" if self.value == option[0] else ""}>{option[1]}</option>"""
+                <option 
+                value="{option[0]}"
+                {" selected" if self.value == option[0] else ""}
+                >{option[1]}</option>"""
         end = f"""
             </select>
         </label>
