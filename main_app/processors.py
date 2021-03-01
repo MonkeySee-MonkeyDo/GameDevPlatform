271ea30668ebfc5befb3cfb24c0f159dd7e44b61
Chai <chaiyawahoo@gmail.com>
2021-03-01 14:58:57 -0800

    fix session timeout

--- main_app/processors.py
+++ main_app/processors.py
@@ -4,6 +4,9 @@ from main_app import app
 
 @app.before_first_request
 def before_first_request():
-    app.permanent_session_lifetime = timedelta(minutes=30)
     if not "logged_in" in session:
         session["logged_in"] = False
+    
+@app.before_request
+def before_request():
+    app.permanent_session_lifetime = timedelta(minutes=30)
